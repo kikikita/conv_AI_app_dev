48ffc50acf0d428e2cf3d8b1d7f5e6fe8f94b274
Nikita <girmanik.nik@gmail.com>
2023-06-17 14:20:35 +0300

    hw2 done

---
+++ notebooks/Task2_1.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import streamlit as st
+import random
+pd.options.mode.chained_assignment = None
+
+
+@st.cache_data
+def get_data():
+    df = pd.read_csv('../data/movies_prepared.csv')
+    return df
+
+
+df = get_data()
+df.dialog = df.dialog.apply(lambda x: eval(x) if isinstance(x, str) else x)
+df.order = df.order.apply(lambda x: eval(x) if isinstance(x, str) else x)
+df.characters = df.characters.apply(
+    lambda x: eval(x) if isinstance(x, str) else x)
+df.genres = df.genres.apply(lambda x: eval(x) if isinstance(x, str) else x)
+df.names = df.names.apply(lambda x: eval(x) if isinstance(x, str) else x)
+
+labels = [
+    'Positive',
+    'Negative',
+    'Neutral'
+]
+mapping = {i: k for i, k in enumerate(labels)}
+
+st.sidebar.header('Выбор фильма и персонажа')
+movie_title = st.sidebar.selectbox('Название фильма',
+                                   df.movie_title.unique().tolist())
+character = st.sidebar.selectbox('Персонаж',
+                                 [' '] + df[
+                                  df.movie_title == movie_title]
+                                 .names.explode().unique().tolist(),
+                                 disabled=False)
+movie_id_ = df.query('movie_title == @movie_title').movie_id.unique().item()
+
+
+def genres_stat(movie_id, character=None):
+    """Функция принимает id фильма и имя персонажа в качестве аргументов,
+    возвращает фрейм данных с частотами эмоций для каждого персонажа в фильме.
+    """
+    df_final = df.query('movie_id == @movie_id')
+    df_final['emotion'] = df_final.dialog.apply(
+        lambda x: [random.randint(0, 2) for _ in x])
+    df_final['emotion'] = df_final.emotion.apply(
+        lambda x: [mapping[i] for i in x])
+    df_final.reset_index(inplace=True, drop=True)
+    df_final = df_final[['names', 'emotion']]
+    df_final = df_final.explode(['names', 'emotion'])
+    df_final = df_final.groupby('names')['emotion'].value_counts(
+        normalize=True).to_frame('frequency')
+    # проверяем персонажа, если это не None, то находим во фрейме данных
+    if character is not None:
+        df_to_return = df_final.loc[character]
+        df_to_return.index.names = ['Эмоция']
+        return df_to_return.rename(columns={
+            'frequency': f'Соотношение для персонажа {character}'})
+    else:
+        df_final.index.names = ['Персонаж', 'Эмоция']
+        return df_final.rename(
+            columns={'frequency': f'Соотношение'})
+
+
+st.header(f'Выбранный фильм: {movie_title}')
+if character == ' ':
+    st.subheader('Персонаж не выбран')
+    st.write(genres_stat(movie_id_))
+else:
+    st.subheader(f'Выбранный персонаж: {character}')
+    st.write(genres_stat(movie_id_, character))
---
+++ notebooks/Task2_2.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.read_csv("movies_prepared.csv")
+
+# Определяем список эмоций
+emotions = ["Positive", "Neutral", "Negative"]
+
+
+# Определяем Streamlit app
+def app():
+    st.title("Приложение для аннотации эмоций в диалоге")
+
+    # Просим пользователя выбрать фильм
+    movies = df["movie_title"].unique()
+    movie = st.selectbox("Выберите фильм", movies, key='movie_select')
+
+    # Фильтруем данные по выбранному фильму
+    data = df[df["movie_title"] == movie].copy()
+
+    # Создаем новую колонку для выбранных эмоций
+    data["emotions"] = [[] for _ in range(len(data))]
+
+    # Отображаем диалоги для выбранного фильма
+    for i, row in data.iterrows():
+        names = eval(row["names"])
+        dialog = eval(row["dialog"])
+        dialogues = [f"{name}: {replic}" for name,
+                     replic in zip(names, dialog)]
+        dialogue_text = "\n\n".join(dialogues)
+        st.write(f"**Dialogue {i + 1}** \n\n {dialogue_text}")
+
+        # Просим пользователя выбрать эмоции для диалога
+        emotions_selected = []
+        for j, replic in enumerate(dialog):
+            emotion = st.multiselect(f"Выберите эмоции для реплик(и) {j+1}",
+                                     emotions, key=f'emotions_select_{i}_{j}')
+            emotions_selected.append(emotion)
+
+        # Отображаем выбранные эмоции
+        st.write("Выбранные эмоции:", emotions_selected)
+
+        # Добавляем выбранные эмоции в датафрейм
+        data.at[i, "emotions"] = emotions_selected
+
+        # Добавляем разделитель
+        st.markdown("---")
+
+    # Добавляем кнопку для загрузки размеченных данных
+    if st.button("Сохранить размеченные данные"):
+        # Сохраняем размеченные данные в csv-файл
+        data.to_csv("my_movies_prepared.csv", index=False)
+
+
+if __name__ == "__main__":
+    app()
